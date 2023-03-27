657e17b555c1fa497f4495c87904163b10a28972
Emily Peregrine <emily.peregrine1@nhs.net>
2023-03-27 17:37:35 +0100

    Initial Commit

---
+++ Peregrine.HelloWorld.Console/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Peregrine.HelloWorld;
+
+var greeter = new Greeter();
+Console.WriteLine(greeter.Greet("Emily"));
---
+++ Peregrine.HelloWorld/Greeter.cs
@@ -0,0 +1,15 @@
+﻿namespace Peregrine.HelloWorld;
+
+public class Greeter
+{
+    private string Prefix { get; }
+    private string Postfix { get; }
+
+    public Greeter(string prefix = "Hello, ", string postfix = "!")
+    {
+        Prefix = prefix;
+        Postfix = postfix;
+    }
+
+    public string Greet (string msg) => $"{Prefix}{msg}{Postfix}";
+}
